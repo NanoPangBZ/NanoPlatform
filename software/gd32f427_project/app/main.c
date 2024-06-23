b2d62cd10ef95938cc23d0d31419583db94d66c1
NanoPangBZ <3096470072@qq.com>
2024-06-23 19:24:00 +0800

    first commit

---
+++ software/gd32f427_project/app/main.c
@@ -0,0 +1,9 @@
+#include "gd32f4xx.h"
+#include <stdio.h>
+#include "main.h"
+
+int main(void)
+{
+    while(1) {
+    }
+}
---
+++ software/gd32f427_project/bootloader/bootloader.c
@@ -0,0 +1,9 @@
+#include "gd32f4xx.h"
+#include <stdio.h>
+#include "bootloader.h"
+
+int main(void)
+{
+    while(1) {
+    }
+}
