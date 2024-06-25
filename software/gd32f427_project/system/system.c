8bfd037edee7a221975d1dda7817dd5504d75619
NanoPangBZ <3096470072@qq.com>
2024-06-26 00:00:28 +0800

    refactor: 🎨为主控定义系统的部分接口

---
+++ software/gd32f427_project/system/system.c
@@ -0,0 +1,20 @@
+#include "system.h"
+
+/**
+ * @brief 获取当前系统时间
+ * @return uint32_t 时间 单位:ms
+ */
+uint32_t get_system_time(void)
+{
+    return 0;
+}
+
+/**
+ * @brief 获取当前运行的固件信息
+ * @param info 存储指针
+ */
+void get_running_elf_info(elf_info_t* info)
+{
+
+}
+
