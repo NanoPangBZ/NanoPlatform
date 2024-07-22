b0d26afa17ef81a5d85c773423042698d54d8252
NanoPangBZ <3096470072@qq.com>
2024-07-22 14:21:28 +0800

    feat: ✨系统心跳灯功能进入系统服务组件

--- software/NanoPlantform/plantform/core/nano_time.c
+++ software/NanoPlantform/plantform/core/nano_time.c
@@ -5,24 +5,29 @@
 static uint32_t sys_tick = 0;
 static nano_io_dev_handle_t led_dev = NULL;
 
+static nano_err_t nano_tick_led_init(void* args)
+{
+    (void)args;
+    led_dev = nano_io_device_open( "debug led" , NANO_IO_READ_WRITE , NANO_BIO );
+    return led_dev ? NANO_OK : NANO_NO_INSTANCE;
+}
+
 static void nano_tick_led_run(void)
 {
-    static uint16_t led_tick = 0;
-    static uint8_t led_on = 0;
-    uint16_t temp;
-    if( !led_dev )
+    if( led_dev != NULL )
     {
-        led_dev = nano_io_device_open( "debug led" , NANO_IO_READ_WRITE , NANO_BIO );
+        static uint16_t led_tick = 0;
+        static uint8_t led_on = 0;
+        uint16_t temp;
+        if( (led_on && led_tick == 100) ||
+            (!led_on && led_tick == 500))
+        {
+            led_tick = 0;
+            led_on = !led_on;
+            nano_io_device_write(led_dev,&led_on,1,&temp);
+        }
+        led_tick++;
     }
-    else if( (led_on && led_tick == 100) || 
-              (!led_on && led_tick == 500))
-    {
-        led_tick = 0;
-        led_on = !led_on;
-        nano_io_device_write(led_dev,&led_on,1,&temp);
-    }
-
-    led_tick++;
 }
 
 uint32_t nano_sys_time_ms(void)
@@ -54,5 +59,5 @@ static nano_err_t nano_plantform_systick_init(void* args)
 #include "nano_func_manager.h"
 
 LOAD_FUNC_TO_FUNC_MANAGER( nano_plantform_systick_init , NANO_PLTFM_PRE_INIT_FUNC_GROUP );
-
+LOAD_FUNC_TO_FUNC_MANAGER( nano_tick_led_init , NANO_PLTFM_SVC_ENTER_FUNC_GRUOP );
 
