3b2932ef86bd267c9f4e0f444ed5bc21030db133
NanoPangBZ <3096470072@qq.com>
2024-11-09 03:14:35 +0800

    chore: 🔧完善tp接口定义

--- software/NanoPlantform/plantform/component/tp/nano_tp.c
+++ software/NanoPlantform/plantform/component/tp/nano_tp.c
@@ -20,8 +20,9 @@ nano_err_t nano_thread_pool_init(void)
     return NANO_NO_IMPL;
 }
 
-nano_tp_pool_handle_t nano_tp_pool_create(void)
+nano_tp_pool_handle_t nano_tp_pool_create(nano_tp_pool_desc_t* desc)
 {
+    (void)desc;
     return NULL;
 }
 
@@ -32,6 +33,12 @@ nano_tp_thread_handle_t nano_tp_thread_create(nano_tp_thread_desc_t* desc)
 }
 
 nano_tp_task_handle_t nano_tp_task_create(nano_tp_task_desc_t* desc)
+{
+    (void)desc;
+    return NULL;
+}
+
+nano_tp_task_handle_t   nano_tp_static_pool_create(const nano_tp_pool_desc_t* desc)
 {
     (void)desc;
     return NULL;
@@ -49,34 +56,110 @@ nano_tp_task_handle_t nano_tp_static_task_create(const nano_tp_task_desc_t* desc
     return NULL;
 }
 
-nano_err_t  nano_tp_pool_bind_thread(nano_tp_pool_handle_t pool,nano_tp_thread_handle_t thread)
+nano_err_t nano_tp_pool_bind_thread(nano_tp_pool_handle_t pool,nano_tp_thread_handle_t thread)
 {
     (void)pool;
     (void)thread;
     return NANO_NO_IMPL;
 }
 
-nano_err_t  nano_tp_pool_unbind_thread(nano_tp_pool_handle_t pool,nano_tp_thread_handle_t thread)
+nano_err_t nano_tp_pool_unbind_thread(nano_tp_pool_handle_t pool,nano_tp_thread_handle_t thread)
 {
     (void)pool;
     (void)thread;
     return NANO_NO_IMPL;
 }
 
-nano_err_t  nano_tp_thread_start(nano_tp_thread_handle_t thread)
+nano_err_t  nano_tp_pool_add_task(nano_tp_pool_handle_t pool,nano_tp_task_handle_t task)
+{
+    (void)pool;
+    (void)task;
+    return NANO_NO_IMPL;
+}
+
+nano_err_t nano_tp_thread_start(nano_tp_thread_handle_t thread)
 {
     (void)thread;
     return NANO_NO_IMPL;
 }
 
-nano_err_t  nano_tp_thread_stop(nano_tp_thread_handle_t thread)
+nano_err_t nano_tp_thread_stop(nano_tp_thread_handle_t thread)
 {
     (void)thread;
     return NANO_NO_IMPL;
 }
 
-nano_err_t  nano_tp_pool_all_thread_start(nano_tp_pool_handle_t pool)
+nano_err_t nano_tp_pool_all_thread_start(nano_tp_pool_handle_t pool)
 {
     (void)pool;
     return NANO_NO_IMPL;
+}
+
+nano_err_t nano_tp_pool_all_thread_stop(nano_tp_pool_handle_t pool)
+{
+    (void)pool;
+    return NANO_NO_IMPL;
+}
+
+nano_err_t nano_tp_pool_bind_thread_with_name(const char* pool_name,const char* thread_name)
+{
+    (void)pool_name;
+    (void)thread_name;
+    return NANO_NO_IMPL;
+}
+
+nano_err_t nano_tp_pool_unbind_thread_with_name(const char* pool_name,const char* thread_name)
+{
+    (void)pool_name;
+    (void)thread_name;
+    return NANO_NO_IMPL;
+}
+
+nano_err_t  nano_tp_pool_add_task_with_name(const char* pool_name,const char* task_name)
+{
+    (void)pool_name;
+    (void)task_name;
+    return NANO_NO_IMPL;
+}
+
+nano_err_t nano_tp_thread_start_with_name(const char* thread_name)
+{
+    (void)thread_name;
+    return NANO_NO_IMPL;
+}
+
+nano_err_t nano_tp_thread_stop_with_name(const char* thread_name)
+{
+    (void)thread_name;
+    return NANO_NO_IMPL;
+}
+
+nano_err_t nano_tp_pool_all_thread_start_with_name(const char* pool_name)
+{
+    (void)pool_name;
+    return NANO_NO_IMPL;
+}
+
+nano_err_t nano_tp_pool_all_thread_stop_with_name(const char* pool_name)
+{
+    (void)pool_name;
+    return NANO_NO_IMPL;
+}
+
+nano_tp_pool_handle_t nano_tp_pool_get_handle(const char* pool_name)
+{
+    (void)pool_name;
+    return NULL;
+}
+
+nano_tp_thread_handle_t nano_tp_thread_get_handle(const char* thread_name)
+{
+    (void)thread_name;
+    return NULL;
+}
+
+nano_tp_task_handle_t nano_tp_task_get_handle(const char* task_name)
+{
+    (void)task_name;
+    return NULL;
 }
--- software/NanoPlantform/plantform/component/tp/nano_tp.h
+++ software/NanoPlantform/plantform/component/tp/nano_tp.h
@@ -18,6 +18,21 @@ typedef struct nano_tp_thread_t* nano_tp_thread_handle_t;
 struct nano_tp_task_t;
 typedef struct nano_tp_task_t* nano_tp_task_handle_t;
 
+//线程池属性
+typedef enum{
+    NANO_TP_POOL_ATTR_DEFAULT = 0x00,                  //默认属性
+    NANO_TP_POOL_ATTR_IMPORTANT = 0x01 << 1,           //重要线程池
+    NANO_TP_POOL_ATTR_REALTIME = 0x01 << 2,            //实时线程池
+    NANO_TP_POOL_ATTR_LONG_CYCLE = 0x01 << 3,          //长周期线程池
+}nano_tp_pool_attr_e;
+typedef uint32_t nano_tp_pool_attr_t;
+
+//线程池描述
+typedef struct{
+    const char* name;
+    nano_tp_pool_attr_t pool_attr;
+}nano_tp_pool_desc_t;
+
 //线程池线程属性
 typedef enum{
     NANO_TP_THREAD_ATTR_DEFAULT = 0x00,                 //默认属性
@@ -49,17 +64,32 @@ typedef struct{
 }nano_tp_task_desc_t;
 
 nano_err_t              nano_thread_pool_init(void);
-nano_tp_pool_handle_t   nano_tp_pool_create(void);
+nano_tp_pool_handle_t   nano_tp_pool_create(nano_tp_pool_desc_t* desc);
 nano_tp_thread_handle_t nano_tp_thread_create(nano_tp_thread_desc_t* desc);
 nano_tp_task_handle_t   nano_tp_task_create(nano_tp_task_desc_t* desc);
+nano_tp_task_handle_t   nano_tp_static_pool_create(const nano_tp_pool_desc_t* desc);
 nano_tp_thread_handle_t nano_tp_static_thread_create(const nano_tp_thread_desc_t* desc);
 nano_tp_task_handle_t   nano_tp_static_task_create(const nano_tp_task_desc_t* desc);
 
 nano_err_t  nano_tp_pool_bind_thread(nano_tp_pool_handle_t pool,nano_tp_thread_handle_t thread);
 nano_err_t  nano_tp_pool_unbind_thread(nano_tp_pool_handle_t pool,nano_tp_thread_handle_t thread);
+nano_err_t  nano_tp_pool_add_task(nano_tp_pool_handle_t pool,nano_tp_task_handle_t task);
 nano_err_t  nano_tp_thread_start(nano_tp_thread_handle_t thread);
 nano_err_t  nano_tp_thread_stop(nano_tp_thread_handle_t thread);
 nano_err_t  nano_tp_pool_all_thread_start(nano_tp_pool_handle_t pool);
+nano_err_t  nano_tp_pool_all_thread_stop(nano_tp_pool_handle_t pool);
+
+nano_err_t  nano_tp_pool_bind_thread_with_name(const char* pool_name,const char* thread_name);
+nano_err_t  nano_tp_pool_unbind_thread_with_name(const char* pool_name,const char* thread_name);
+nano_err_t  nano_tp_pool_add_task_with_name(const char* pool_name,const char* task_name);
+nano_err_t  nano_tp_thread_start_with_name(const char* thread_name);
+nano_err_t  nano_tp_thread_stop_with_name(const char* thread_name);
+nano_err_t  nano_tp_pool_all_thread_start_with_name(const char* pool_name);
+nano_err_t  nano_tp_pool_all_thread_stop_with_name(const char* pool_name);
+
+nano_tp_pool_handle_t nano_tp_pool_get_handle(const char* pool_name);
+nano_tp_thread_handle_t nano_tp_thread_get_handle(const char* thread_name);
+nano_tp_task_handle_t nano_tp_task_get_handle(const char* task_name);
 
 #ifdef __cplusplus
 }
