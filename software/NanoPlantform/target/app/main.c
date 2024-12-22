4b3de0dfedba46c2560dfb52dff7187edce1c87f
NanoPangBZ <3096470072@qq.com>
2024-12-22 18:09:40 +0800

    test: ✅隔离线程池与调度器的测试代码

--- software/NanoPlantform/target/app/main.c
+++ software/NanoPlantform/target/app/main.c
@@ -30,13 +30,25 @@ static void thread_func2(void* args)
     }
 }
 
-int main(void)
+static void thread_schudler_test(void)
 {
     nano_plantform_init();
 
     nano_io_device_open("debug led",NANO_IO_READ_WRITE,NANO_BIO,&led_handle);
     nano_thread_create( NULL , "Test" , thread_func , (void*)led_handle , NANO_THRAD_MID_PRIORITY , NANO_THREAD_BIG_STACK_SIZE );
     nano_thread_create( NULL , "Test" , thread_func2 , NULL , NANO_THRAD_MID_PRIORITY , NANO_THREAD_BIG_STACK_SIZE );
+
+    nano_plantform_start();
+}
+
+static nano_err_t nano_tp_task_func(void* args)
+{
+    return NANO_OK;
+}
+
+static void nano_thread_pool_test(void)
+{
+    nano_plantform_init();
 
     nano_tp_init();
 
@@ -47,14 +59,27 @@ int main(void)
 
     pool_desc.name = "test";
     thread_desc.name = "test";
+
     task_desc.name = "test";
+    task_desc.cycle_ms = 1000;
+    task_desc.task_func = nano_tp_task_func;
+    task_desc.task_attr = NANO_TP_TASK_ATTR_DEFAULT;
+    task_desc.user_ctx = NULL;
 
     nano_tp_pool_handle_t pool =  nano_tp_pool_create(&pool_desc);
     nano_tp_thread_handle_t thread = nano_tp_thread_create(&thread_desc);
     nano_tp_pool_bind_thread( pool , thread );
     nano_tp_pool_add_task( pool , &task_handle , &task_desc );
 
+    nano_tp_pool_start(pool);
+
     nano_plantform_start();
+}
+
+int main(void)
+{
+    // thread_schudler_test();
+    nano_thread_pool_test();
     while(1);
     return -1;
 }
