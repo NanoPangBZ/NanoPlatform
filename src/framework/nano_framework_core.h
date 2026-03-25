e422bcfd5092c2f672c6ad19bf7938ae1a60bed4
NanoPangBZ <codingbugstd@gmail.com>
2026-03-25 18:16:01 +0800

    perf: 👌优化systick注册段结构

--- src/framework/nano_framework_core.h
+++ src/framework/nano_framework_core.h
@@ -2,12 +2,16 @@
 
 #include <stdint.h>
 
+#pragma pack(1)
+
 // SysTick函数注册项结构体
 typedef struct systick_function_item_t{
     const char* name;
     void (*function)(void);
     uint32_t reserved:32; // Reserved for future use, ensuring 32-bit alignment
 }systick_function_item_t;
+
+#pragma pack()
 
 /**
  * @brief 定义Systick函数注册项的宏
@@ -29,6 +33,12 @@ extern "C" {
  * @brief 进入框架核心函数
 */
 void enter_nano_framework( void );
+
+/**
+ * @brief 获取系统运行的毫秒数
+ * @return 系统运行的毫秒数
+*/
+uint32_t nano_framework_time_ms(void);
 
 #ifdef __cplusplus
 }
