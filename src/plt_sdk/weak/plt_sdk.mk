466b3e4127e015410ecb878170dda52fccb69bf9
NanoPangBZ <codingbugstd@gmail.com>
2026-03-27 02:10:31 +0800

    feat: ✨添加arch_cpu接口定义及其弱实现

--- src/plt_sdk/weak/plt_sdk.mk
+++ src/plt_sdk/weak/plt_sdk.mk
@@ -5,3 +5,4 @@ PLT_SDK_SRCS += $(PLT_WEAK_SDK_DIR)arch_init_weak.c
 PLT_SDK_SRCS += $(PLT_WEAK_SDK_DIR)arch_uart_weak.c
 PLT_SDK_SRCS += $(PLT_WEAK_SDK_DIR)arch_gpio_weak.c
 PLT_SDK_SRCS += $(PLT_WEAK_SDK_DIR)arch_spi_weak.c
+PLT_SDK_SRCS += $(PLT_WEAK_SDK_DIR)arch_cpu_weak.c
