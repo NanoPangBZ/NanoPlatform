deb3fd7fd6d155cf50ecaeb5f717132fc02e9a88
NanoPangBZ <codingbugstd@gmail.com>
2026-08-24 19:26:05 +0800

    feat: ✨优化arch_spi接口设计

--- src/arch/arch_spi.h
+++ src/arch/arch_spi.h
@@ -5,8 +5,13 @@
 //arch spi port
 typedef uint8_t arch_spi_port_t;
 
-typedef void (*arch_spi_send_callback_t)( void* ctx );
-typedef void (*arch_spi_receive_callback_t)( void* ctx , const uint8_t* data , uint32_t len);
+/**
+ * @brief SPI传输回调函数
+ * @param ctx 上下文指针
+ * @param recieve_data 接收数据指针
+ * @param len 数据长度，单位字节
+*/
+typedef void (*arch_spi_transfer_cb_t)( void* ctx , uint8_t* recieve_data , uint32_t len );
 
 #ifdef __cplusplus
 extern "C" {
@@ -26,60 +31,28 @@ void arch_spi_init( arch_spi_port_t port , uint32_t speed );
 void arch_spi_deinit( arch_spi_port_t port );
 
 /**
- * @brief SPI发送数据
+ * @brief SPI传输数据
  * @param port SPI端口
- * @param data 数据指针
- * @param length 数据长度，单位字节
- * @param timeout_ms 发送超时时间，单位毫秒
- * @return 实际发送的数据长度，单位字节
- * @note 该函数为阻塞式调用，直到数据发送完成或发生错误才会返回
+ * @param send_data 发送数据指针
+ * @param receive_data 接收数据指针
+ * @param len 数据长度，单位字节
+ * @return 实际传输的数据长度，单位字节
+ * @note 该函数用于传输数据，阻塞式传输
 */
-uint32_t arch_spi_send( arch_spi_port_t port , const uint8_t* data , uint32_t length , uint32_t timeout_ms );
+uint32_t arch_spi_transfer( arch_spi_port_t port , const uint8_t* send_data , uint8_t* receive_data , uint32_t len);
 
 /**
- * @brief SPI接收数据
+ * @brief SPI传输数据，非阻塞式传输
  * @param port SPI端口
- * @param data 数据指针，读取的数据将存储在此处
- * @param length 数据长度，单位字节
- * @param timeout_ms 读取超时时间，单位毫秒
- * @return 实际读取的数据长度，单位字节
- * @note 该函数为阻塞式调用，直到读取到数据或发生错误才会返回
+ * @param send_data 发送数据指针
+ * @param receive_data 接收数据指针
+ * @param len 数据长度，单位字节
+ * @param callback 传输回调函数
+ * @param ctx 上下文指针
+ * @return 实际传输的数据长度，单位字节
+ * @note 该函数用于传输数据，非阻塞式传输
 */
-uint32_t arch_spi_recieve( arch_spi_port_t port , uint8_t* data , uint32_t length , uint32_t timeout_ms );
-
-/**
- * @brief 设置SPI发送完成回调函数
- * @param port SPI端口
- * @param callback 发送完成回调函数
- * @param ctx 回调函数的上下文指针
-*/
-void arch_spi_set_send_complete_callback( arch_spi_port_t port , arch_spi_send_callback_t callback , void* ctx );
-
-/**
- * @brief 设置SPI接收回调函数
- * @param port SPI端口
- * @param callback 接收回调函数
- * @param ctx 回调函数的上下文指针
-*/
-void arch_spi_set_receive_callback( arch_spi_port_t port , arch_spi_receive_callback_t callback , void* ctx );
-
-/**
- * @brief 启动SPI发送
- * @param port SPI端口
- * @param data 数据指针
- * @param length 数据长度，单位字节
- * @note 该函数用于启动SPI的发送功能，通常在设置发送完成回调函数后调用，以便开始发送数据并触发回调函数
-*/
-void arch_spi_start_send( arch_spi_port_t port , const uint8_t* data , uint32_t length );
-
-/**
- * @brief 启动SPI接收
- * @param port SPI端口
- * @param data 数据指针，读取的数据将存储在此处
- * @param length 数据长度，单位字节
- * @note 该函数用于启动SPI的接收功能，通常在设置接收回调函数后调用，以便开始接收数据并触发回调函数
-*/
-void arch_spi_start_receive( arch_spi_port_t port , uint8_t* data , uint32_t length );
+uint32_t arch_spi_transfer_non_blocking( arch_spi_port_t port , const uint8_t* send_data , uint8_t* receive_data , uint32_t len, arch_spi_transfer_cb_t callback , void* ctx);
 
 #ifdef __cplusplus
 }
--- src/arch/weak/arch_spi_weak.c
+++ src/arch/weak/arch_spi_weak.c
@@ -19,48 +19,22 @@ __attribute__((weak)) void arch_spi_deinit( arch_spi_port_t port )
     (void)port;
 }
 
-__attribute__((weak)) uint32_t arch_spi_send( arch_spi_port_t port , const uint8_t* data , uint32_t length , uint32_t timeout_ms )
+__attribute__((weak)) uint32_t arch_spi_transfer( arch_spi_port_t port , const uint8_t* send_data , uint8_t* receive_data , uint32_t len )
 {
     (void)port;
-    (void)data;
-    (void)length;
-    (void)timeout_ms;
+    (void)send_data;
+    (void)receive_data;
+    (void)len;
     return 0;
 }
 
-__attribute__((weak)) uint32_t arch_spi_recieve( arch_spi_port_t port , uint8_t* data , uint32_t length , uint32_t timeout_ms )
+__attribute__((weak)) uint32_t arch_spi_transfer_non_blocking( arch_spi_port_t port , const uint8_t* send_data , uint8_t* receive_data , uint32_t len , arch_spi_transfer_cb_t callback , void* ctx )
 {
     (void)port;
-    (void)data;
-    (void)length;
-    (void)timeout_ms;
+    (void)send_data;
+    (void)receive_data;
+    (void)len;
+    (void)callback;
+    (void)ctx;
     return 0;
-}
-
-__attribute__((weak)) void arch_spi_set_send_complete_callback( arch_spi_port_t port , arch_spi_send_callback_t callback , void* ctx )
-{
-    (void)port;
-    (void)callback;
-    (void)ctx;
-}
-
-__attribute__((weak)) void arch_spi_set_receive_callback( arch_spi_port_t port , arch_spi_receive_callback_t callback , void* ctx )
-{
-    (void)port;
-    (void)callback;
-    (void)ctx;
-}
-
-__attribute__((weak)) void arch_spi_start_send( arch_spi_port_t port , const uint8_t* data , uint32_t length )
-{
-    (void)port;
-    (void)data;
-    (void)length;
-}
-
-__attribute__((weak)) void arch_spi_start_receive( arch_spi_port_t port , uint8_t* data , uint32_t length )
-{
-    (void)port;
-    (void)data;
-    (void)length;
 }
